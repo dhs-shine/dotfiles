cfc3d876dfa4f1a25f6dddc2a6760644be3a1048
dhs-shine <dhs.shine@gmail.com>
2015-10-09 22:26:06 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-cp .vimrc .bashrc .dircolors ~/
-cp -R .vim ~/
+if [ ! "which vim" ]; then
+	echo "vim is not installed. Install vim."
+	sudo apt-get update
+	sudo apt-get install vim
+fi
+echo "copy .bashrc .dircolors .vim to home directory."
+cp .vimrc .bashrc .dircolors /home/${USER}/
+cp -R .vim /home/${USER}/
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+vundle="/home/${USER}/.vim/bundle/Vundle.vim"
+if [[ ! -e "$vundle" ]]; then
+	echo "Clone Vundle.vim.git and install all plugins"
+	git clone https://github.com/gmarik/Vundle.vim.git $vundle
+fi
 vim +PluginInstall +qall
+echo "Done."
